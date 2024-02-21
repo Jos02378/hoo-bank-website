37906c5d4032ae4cc9ee84591b8fb41476eacc76
Joseph Haryanto <57058570+Jos02378@users.noreply.github.com>
2024-02-22 00:25:22 +0700

    feat: modify clients component

--- bank_modern_app/src/components/Clients.tsx
+++ bank_modern_app/src/components/Clients.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import { clients } from '../constants';
 
-const Clients = () => {
-  return <div>Clients</div>;
-};
+const Clients = () => (
+  <section id='clients' className='flex-center my-4'>
+    <div className='flex-center flex-wrap w-full'>
+      {clients.map((client) => (
+        <div
+          key={client.id}
+          className='flex-1 flex-center min-w-[120px] sm:min-w-[192px] '
+        >
+          <img
+            src={client.logo}
+            alt='client'
+            className='sm:w-[192px] w-[100px] object-contain'
+          />
+        </div>
+      ))}
+    </div>
+  </section>
+);
 
 export default Clients;
