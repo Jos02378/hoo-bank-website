6cf23e164249c5c5d9a4bcb8b10cd0d7235ba02c
Joseph Haryanto <57058570+Jos02378@users.noreply.github.com>
2024-02-22 00:25:35 +0700

    feat: modify CTA component

--- bank_modern_app/src/components/CTA.tsx
+++ bank_modern_app/src/components/CTA.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import Button from './Button';
 
-const CTA = () => {
-  return <div>CTA</div>;
-};
+const CTA = () => (
+  <section className='flex-center margin-y padding flex-col bg-black-gradient-2 rounded-[20px] box-shadow sm:flex-row'>
+    <div className='flex-1 flex flex-col'>
+      <h2 className='heading-secondary'>Let’s try our service now!</h2>
+      <p className='paragraph max-w-[470px] mt-5'>
+        Everything you need to accept card payments and grow your business
+        anywhere on the planet.
+      </p>
+    </div>
+
+    <div className='flex-center ml-0 mt-10 sm:ml-10 sm:mt-0'>
+      <Button />
+    </div>
+  </section>
+);
 
 export default CTA;
