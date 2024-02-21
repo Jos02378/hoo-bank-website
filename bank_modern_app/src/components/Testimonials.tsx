f4fa3a22b826fac5f0a586d0b29ecb100aa765c8
Joseph Haryanto <57058570+Jos02378@users.noreply.github.com>
2024-02-22 00:23:21 +0700

    feat: modify testimonials section

--- bank_modern_app/src/components/Testimonials.tsx
+++ bank_modern_app/src/components/Testimonials.tsx
@@ -1,7 +1,29 @@
 import React from 'react';
+import { feedback } from '../constants';
+import FeedbackCard from './FeedbackCard';
 
-const Testimonials = () => {
-  return <div>Testimonials</div>;
-};
+const Testimonials = () => (
+  <section id='client' className='padding-y flex-center flex-col relative'>
+    <div className='absolute z-0 w-[60%] h-[60%] -right-[50%] rounded-full blue__gradient' />
+
+    <div className='w-full flex justify-between items-center flex-col mb-6 relative z-[1] sm:mb-16 md:flex-row'>
+      <h1 className='heading-secondary'>
+        What people are <br className='hidden sm:block' /> saying about us
+      </h1>
+      <div className='w-full mt-6 md:mt-0'>
+        <p className='paragraph text-left max-w-[450px]'>
+          Everything you need to accept card payments and grow your business
+          anywhere on the planet.
+        </p>
+      </div>
+    </div>
+
+    <div className='flex flex-wrap justify-center w-full feedback-container relative z-[1] sm:justify-start'>
+      {feedback.map((card) => (
+        <FeedbackCard key={card.id} {...card} />
+      ))}
+    </div>
+  </section>
+);
 
 export default Testimonials;
