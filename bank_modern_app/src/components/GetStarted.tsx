5758170f844603411620f5a5383c16778dc5730b
Joseph Haryanto <57058570+Jos02378@users.noreply.github.com>
2024-02-22 00:24:32 +0700

    feat: wrap GetStarted component with anchor tag

--- bank_modern_app/src/components/GetStarted.tsx
+++ bank_modern_app/src/components/GetStarted.tsx
@@ -2,23 +2,25 @@ import React from 'react';
 import arrowup from '../assets/arrow-up.svg';
 
 const GetStarted = () => (
-  <div className='flex-center w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer'>
-    <div className='flex-center flex-col bg-primary w-[100%] h-[100%] rounded-full'>
-      <div className='flex-start flex-row'>
-        <p className='font-poppins font-medium text-[18px] leading-[23px] mr-2'>
-          <span className='text-gradient'>Get</span>
+  <a href='#features'>
+    <div className='flex-center w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer'>
+      <div className='flex-center flex-col bg-primary w-[100%] h-[100%] rounded-full'>
+        <div className='flex-start flex-row'>
+          <p className='font-poppins font-medium text-[18px] leading-[23px] mr-2'>
+            <span className='text-gradient'>Get</span>
+          </p>
+          <img
+            src={arrowup}
+            alt='arrow'
+            className='w-[23px] h-[23px] object-contain'
+          />
+        </div>
+        <p className='font-poppins font-medium text-[18px] leading-[23px]'>
+          <span className='text-gradient'>Started</span>
         </p>
-        <img
-          src={arrowup}
-          alt='arrow'
-          className='w-[23px] h-[23px] object-contain'
-        />
       </div>
-      <p className='font-poppins font-medium text-[18px] leading-[23px]'>
-        <span className='text-gradient'>Started</span>
-      </p>
     </div>
-  </div>
+  </a>
 );
 
 export default GetStarted;
