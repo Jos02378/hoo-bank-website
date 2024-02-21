d4825e4a33465f2f3891116b5d3e5d67ef42d717
Joseph Haryanto <57058570+Jos02378@users.noreply.github.com>
2024-02-21 22:13:14 +0700

    feat: create initial components

--- bank_modern_app/src/App.tsx
+++ bank_modern_app/src/App.tsx
@@ -1,5 +1,37 @@
 import React from 'react';
+import {
+  Navbar,
+  Hero,
+  Stats,
+  Business,
+  Billing,
+  CardDeal,
+  Testimonials,
+  Clients,
+  CTA,
+  Footer,
+} from './components';
 
-const App = () => <div>React App</div>;
+const App = () => (
+  <div className='bg-primary w-full overflow-hidden'>
+    <div className='padding-x flexCenter'></div>
+    <div className='box-width mx-auto'>
+      <Navbar />
+    </div>
+
+    <div className='bg-primary flex-start'>
+      <div className='box-width'>
+        <Hero />
+      </div>
+    </div>
+
+    <div className='bg-primary padding-x flex-start'>
+      <div className='box-width'>
+        <Stats /> <Business /> <Billing /> <CardDeal /> <Testimonials />{' '}
+        <Clients /> <CTA /> <Footer />
+      </div>
+    </div>
+  </div>
+);
 
 export default App;
