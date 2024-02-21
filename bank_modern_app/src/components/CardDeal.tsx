a996464d91320eb88a116543d0e100ae1b60c451
Joseph Haryanto <57058570+Jos02378@users.noreply.github.com>
2024-02-22 00:25:10 +0700

    feat: modify card deal component

--- bank_modern_app/src/components/CardDeal.tsx
+++ bank_modern_app/src/components/CardDeal.tsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import card from '../assets/card.png';
+import Button from './Button';
 
-const CardDeal = () => {
-  return <div>CardDeal</div>;
-};
+const CardDeal = () => (
+  <section className='section'>
+    <div className='section-info'>
+      <h2 className='heading-secondary'>
+        Find a better card deal <br className='sm:block hidden' /> in few easy
+        steps.
+      </h2>
+      <p className='paragraph max-w-[480px] mt-5'>
+        Unlock financial empowerment with our versatile App. Seamlessly track
+        stock market trends and discover exclusive credit card deals, all in one
+        convenient platform.
+      </p>
+      <Button styles='mt-10'></Button>
+    </div>
+
+    <div className='section-img'>
+      <img src={card} alt='card' className='w-[100%] h-[100%]' />
+    </div>
+  </section>
+);
 
 export default CardDeal;
